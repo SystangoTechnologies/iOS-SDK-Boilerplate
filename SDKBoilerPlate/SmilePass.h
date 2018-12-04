13610f27f22b7c8acae66adae46f248b4e4c13c3
Rajat Pagare <rpagare@isystango.com>
2018-12-04 18:21:03 +0530

    add xcuserspacedata

--- SDKBoilerPlate/SmilePass.h
+++ SDKBoilerPlate/SmilePass.h
@@ -17,3 +17,4 @@ FOUNDATION_EXPORT const unsigned char SDKBoilerPlateVersionString[];
 // In this header, you should import all the public headers of your framework using statements like #import <SDKBoilerPlate/PublicHeader.h>
 
 
+
